1e1e7efbedfb783ad537e3ed85666d4841d5aa6d
ivanvasilevskij <i.vaievskiy@dats.team>
2023-09-04 11:40:24 +0300

    [feature]

--- src/main/resources/scripts/00_init.sql
+++ src/main/resources/scripts/00_init.sql
@@ -75,3 +75,26 @@ create table "introduce_info"
     "description"  text        not null,
     "image_base64" text
 );
+
+create table "user"
+(
+    "id"               bigserial primary key,
+    "uuid"             uuid unique not null,
+    "login"            text        not null,
+    "encoded_password" text        not null,
+    "name"             text        not null,
+    "date_of_birth"    date        not null,
+    "sex"              text        not null
+);
+
+create table "refresh_token"
+(
+    "id"             bigserial primary key,
+    "user_id"        bigint                      not null,
+    "refresh_token"  text unique                 not null,
+    "created_at"     timestamp without time zone not null,
+    "expired_at"     timestamp without time zone not null,
+    "deactivated_at" timestamp without time zone,
+
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
--- src/test/java/com/recycleforlife/AppTest.java
+++ src/test/java/com/recycleforlife/AppTest.java
@@ -1,13 +1,16 @@
 package com.recycleforlife;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @ActiveProfiles("test")
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
 class AppTest {
 
     @DynamicPropertySource
@@ -17,6 +20,9 @@ class AppTest {
 
     @Test
     void context() {
-
+        System.out.println(PostgresTestContainer.POSTGRES_CONTAINER.getJdbcUrl());
+        System.out.println(PostgresTestContainer.POSTGRES_CONTAINER.getUsername());
+        System.out.println(PostgresTestContainer.POSTGRES_CONTAINER.getPassword());
+        System.out.println("");
     }
 }
